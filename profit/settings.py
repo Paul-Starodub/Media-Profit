77ec722a638014d974b930b6ea64fdc9e7ff56ff
Paul Starodub <amorallex@gmail.com>
2023-09-28 16:11:49 +0300

    Updated settings.py with ddtoolbar

--- profit/settings.py
+++ profit/settings.py
@@ -32,6 +32,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 # Application definition
 
@@ -43,12 +47,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "debug_toolbar",
     "spend.apps.SpendConfig",
     "revenue.apps.RevenueConfig",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
