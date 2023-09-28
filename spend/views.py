6f5f24ad7737a0b68c34a475415a79a6d97ee60a
Paul Starodub <amorallex@gmail.com>
2023-09-28 16:10:58 +0300

    Updated all views

--- spend/views.py
+++ spend/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from typing import Optional
 
-# Create your views here.
+from django.db.models import Sum, Subquery, OuterRef
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from revenue.models import RevenueStatistic
+from spend.models import SpendStatistic
+from spend.serializers import SpendStatisticSerializer
+
+
+class SpendStatisticsView(APIView):
+    def get(self, request: Request, format: Optional[str] = None) -> Response:
+        subquery = (
+            RevenueStatistic.objects.filter(spend=OuterRef("pk"))
+            .values("spend")
+            .annotate(total_revenue=Sum("revenue"))
+            .values("total_revenue")[:1]
+        )
+
+        queryset = SpendStatistic.objects.values("date", "name").annotate(
+            total_spend=Sum("spend"),
+            total_impressions=Sum("impressions"),
+            total_clicks=Sum("clicks"),
+            total_conversion=Sum("conversion"),
+            total_revenue=Subquery(subquery),
+        )
+
+        serializer = SpendStatisticSerializer(queryset, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
